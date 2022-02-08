9e642f6d23ef7a004d3af7ad4ec21594fbac1138
zim0369 <zim@onionmail.org>
2022-02-08 20:39:48 +0530

    view imgur history, remove images, open images

--- imgur.sh
+++ imgur.sh
@@ -13,45 +13,101 @@ default_client_id=c9a6efb3d7932fd
 client_id="${IMGUR_CLIENT_ID:=$default_client_id}"
 
 # Function to output usage instructions
-function usage {
-	echo "Usage: $(basename $0) [<filename|URL> [...]]" >&2
-	echo
-	echo "Upload images to imgur and output their new URLs to stdout. Each one's" >&2
-	echo "delete page is output to stderr between the view URLs." >&2
-	echo
-	echo "A filename can be - to read from stdin. If no filename is given, stdin is read." >&2
-	echo
-	echo "If xsel, xclip, pbcopy, or clip is available," >&2
-	echo "the URLs are put on the X selection or clipboard for easy pasting." >&2
-	echo "Use environment variables to set special options for your clipboard program (see code)." >&2
+usage () {
+	cat << EOF
+     
+USAGE:
+    $(basename "$0") [<filename|URL> [...]]
+    ${0##*/} [OPTIONS] [NUMBER]
+
+OPTIONS:
+    -h, --help              Show this help message
+    -l, --list              List imgur history of your uploads
+    -o, --open <NUM>        Open recent <NUM> images(<NUM> is optional)
+    -r, --remove <NUM>      Remove recent <NUM> images from imgur(<NUM> is optional)
+     
+DESCRIPTION: 
+    Upload images to imgur and output their new URLs to stdout. Each one's
+    delete page is output to stderr between the view URLs.
+     
+    A filename can be - to read from stdin. If no filename is given, stdin is read.
+    If xsel, xclip, pbcopy, or clip is available,
+    the URLs are put on the X selection or clipboard for easy pasting.
+     
+    Use environment variables to set special options for your clipboard program (see
+    code).
+    NAME 
+    Randomly select & apply an kitty color theme
+
+EOF
+ 
 }
 
 # Function to upload a path
 # First argument should be a content spec understood by curl's -F option
-function upload {
-	curl -s -H "Authorization: Client-ID $client_id" -H "Expect: " -F "image=$1" https://api.imgur.com/3/image.xml
+upload () {
+	curl --progress-bar -H "Authorization: Client-ID $client_id" \
+        -H "Expect: " -F "image=$1" https://api.imgur.com/3/image.xml
 	# The "Expect: " header is to get around a problem when using this through
 	# the Squid proxy. Not sure if it's a Squid bug or what.
 }
-
+ 
+list () {
+    less ~/imgur.txt
+} 
+ 
+open () {
+    if [ -n "$1" ]; then
+        for each_image in $( grep -io 'https://i\..*' imgur.txt  | tail "-$1" ); do
+            xdg-open "$each_image"
+        done 
+    else
+        xdg-open "$( grep -io 'https://i\..*' imgur.txt  | tail -1 )"
+    fi
+}
+ 
+remove () {
+    if [ -n "$1" ]; then
+        for hash in $( sed -n -e 's/^.*delete\///p' imgur.txt | tail "-$1" ); do
+            curl --location -g --request DELETE "https://api.imgur.com/3/image/$hash" \
+                --header "Authorization: Client-ID $client_id"
+            done 
+        else
+            hash=$( sed -n -e 's/^.*delete\///p' imgur.txt | tail -1 )
+            curl --location -g --request DELETE "https://api.imgur.com/3/image/$hash" \
+                --header "Authorization: Client-ID $client_id"
+    fi
+}
+ 
 # Check arguments
-if [ "$1" == "-h" -o "$1" == "--help" ]; then
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 	usage
 	exit 0
+elif [ "$1" == "-l" ] || [ "$1" == "--list" ]; then
+	list
+	exit 0
+elif [ "$1" == "-o" ] || [ "$1" == "--open" ]; then
+    open "$2" 
+	exit 0
+elif [ "$1" == "-r" ] || [ "$1" == "--remove" ]; then
+    remove "$2" 
+	exit 0
 elif [ $# -eq 0 ]; then
-	echo "No file specified; reading from stdin" >&2
+	printf '%s\n' "No file specified; reading from stdin" >&2
 	exec "$0" -
 fi
 
 # Check curl is available
 type curl &>/dev/null || {
-	echo "Couldn't find curl, which is required." >&2
+	printf '%s\n' "Couldn't find curl, which is required." >&2
 	exit 17
 }
 
 clip=""
 errors=false
 
+printf '%s\n' "TIME:   $( date )" >> "$HOME/imgur.txt"
+     
 # Loop through arguments
 while [ $# -gt 0 ]; do
 	file="$1"
@@ -64,8 +120,9 @@ while [ $# -gt 0 ]; do
 	else
 		# File -> imgur
 		# Check file exists
-		if [ "$file" != "-" -a ! -f "$file" ]; then
-			echo "File '$file' doesn't exist; skipping" >&2
+		if [ "$file" != "-" ] && [ ! -f "$file" ]; then
+			printf '%s\n' "File '$file' doesn't exist; skipping" >&2
+            printf '%s\n' "ERROR:  $file doesn't exist" >> "$HOME/imgur.txt"
 			errors=true
 			continue
 		fi
@@ -73,13 +130,15 @@ while [ $# -gt 0 ]; do
 	fi
 
 	if [ $? -ne 0 ]; then
-		echo "Upload failed" >&2
+		printf '%s\n' "Upload failed" >&2
+        printf '%s\n' "ERROR:  Upload failed" >> "$HOME/imgur.txt"
 		errors=true
 		continue
-	elif echo "$response" | grep -q 'success="0"'; then
-		echo "Error message from imgur:" >&2
+	elif printf '%s\n' "$response" | grep -q 'success="0"'; then
+		printf '%s\n' "Error message from imgur:" >&2
 		msg="${response##*<error>}"
-		echo "${msg%%</error>*}" >&2
+		printf '%s\n' "${msg%%</error>*}" >&2
+        printf '%s\n' "ERROR:  ${msg%%</error>*}" >> "$HOME/imgur.txt"
 		errors=true
 		continue
 	fi
@@ -89,8 +148,11 @@ while [ $# -gt 0 ]; do
 	url="${url%%</link>*}"
 	delete_hash="${response##*<deletehash>}"
 	delete_hash="${delete_hash%%</deletehash>*}"
-	echo $url
-	echo "Delete page: https://imgur.com/delete/$delete_hash" >&2
+	printf '%s\n' "$url"
+	printf '%s\n' "Delete page: https://imgur.com/delete/$delete_hash" >&2
+    printf '%s\n' "FILE:   $file
+LINK:   $url
+DELETE: https://imgur.com/delete/$delete_hash" >> "$HOME/imgur.txt"
 
 	# Append the URL to a string so we can put them all on the clipboard later
 	clip+="$url"
@@ -98,17 +160,21 @@ while [ $# -gt 0 ]; do
 		clip+=$'\n'
 	fi
 done
-
+ 
+printf '\n%s\n\n' \
+"----------------------------------------------------------------------" >> \
+"$HOME/imgur.txt"
+ 
 # Put the URLs on the clipboard if we can
 if type pbcopy &>/dev/null; then
-	echo -n "$clip" | pbcopy $IMGUR_PBCOPY_OPTIONS
+	printf '%s' "$clip" | pbcopy $IMGUR_PBCOPY_OPTIONS
 elif type clip &>/dev/null; then
-	echo -n "$clip" | clip $IMGUR_CLIP_OPTIONS
-elif [ $DISPLAY ]; then
+	printf '%s' "$clip" | clip $IMGUR_CLIP_OPTIONS
+elif [ "$DISPLAY" ]; then
 	if type xsel &>/dev/null; then
-		echo -n "$clip" | xsel -i $IMGUR_XSEL_OPTIONS
+		printf '%s' "$clip" | xsel -i $IMGUR_XSEL_OPTIONS
 	elif type xclip &>/dev/null; then
-		echo -n "$clip" | xclip $IMGUR_XCLIP_OPTIONS
+		printf '%s' "$clip" | xclip $IMGUR_XCLIP_OPTIONS
 	else
 		echo "Haven't copied to the clipboard: no xsel or xclip" >&2
 	fi
@@ -117,5 +183,5 @@ else
 fi
 
 if $errors; then
-	exit 1
+    exit 1
 fi
